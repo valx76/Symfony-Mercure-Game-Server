09d98423772853ff4d7ae5785928b9e2ae7024ba
Valentin PONS <valx76@gmail.com>
2025-07-19 21:16:14 +0200

    Set the GenerateWorld command invokable (Symfony 7.3)

--- src/Game/Presentation/Console/GenerateWorldCommand.php
+++ src/Game/Presentation/Console/GenerateWorldCommand.php
@@ -5,9 +5,9 @@ namespace App\Game\Presentation\Console;
 use App\Game\Domain\Model\Entity\World;
 use App\Game\Domain\Model\Repository\WorldRepositoryInterface;
 use App\SharedContext\Domain\Service\UuidGeneratorInterface;
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -24,27 +24,17 @@ final class GenerateWorldCommand extends Command
         parent::__construct();
     }
 
-    protected function configure(): void
+    public function __invoke(InputInterface $input, OutputInterface $output, #[Argument] string $name): int
     {
-        $this
-            ->addArgument('name', InputArgument::REQUIRED, 'World\'s name')
-        ;
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        /** @var string $worldName */
-        $worldName = $input->getArgument('name');
-
         $world = new World(
             $this->uuidGenerator->generate(),
-            $worldName,
+            $name,
             [],
         );
         $this->worldRepository->save($world);
 
         $output->writeln(
-            sprintf('The new world "%s" has been generated.', $worldName)
+            sprintf('The new world "%s" has been generated.', $name)
         );
 
         return Command::SUCCESS;
