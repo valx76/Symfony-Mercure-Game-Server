5cf465b582d9131f4d2e51b63e25e00e1a46fdd9
Valentin PONS <valx76@gmail.com>
2025-07-28 11:42:32 +0200

    Fixed level1 -> It was spawning on a collision position

--- src/Game/Domain/Model/Entity/Level/Level1.php
+++ src/Game/Domain/Model/Entity/Level/Level1.php
@@ -15,7 +15,7 @@ final readonly class Level1 implements LevelInterface
         return [
             1, 1, 1, 1, 1,
             1, 1, 0, 0, 1,
-            1, 1, 0, 0, 1,
+            1, 0, 0, 0, 1,
             1, 0, 0, 0, 1,
             1, 1, 1, 1, 1,
         ];
